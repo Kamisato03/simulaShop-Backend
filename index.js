6de71be837061471579c22f653d8ded40acaebf5
kamisato03 <angmejia0324@gmail.com>
2024-08-04 17:31:26 -0600

    error de cors

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import "dotenv/config";
 import "./database/connectdb.js";
 import express from "express";
+import cors from "cors";
 import storeRoutes from "./routes/store.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import baseProductRoutes from "./routes/baseProduct.routes.js";
 
+const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2];
 const app = express();
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || whiteList.includes(origin)) {
+        return callback(null, origin);
+      }
+      return callback("Error de CORS origin: " + origin + " No autorizado!");
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
